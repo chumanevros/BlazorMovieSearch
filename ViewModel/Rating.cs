989246523e6339e63458eb1ed8b6c8341ecc9d66
Cornel Human <cornel.human@evros.ie>
2019-10-08 11:44:01 +0100

    Initial commit

---
+++ ViewModel/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovieOana.ViewModel
+{
+    [Serializable]
+    public class Rating
+    {
+        public string Source { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ ViewModel/SearchResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovieOana.ViewModel
+{
+    [Serializable]
+    public class SearchResult
+    {
+            public List<Search> Search { get; set; }
+            public string totalResults { get; set; }
+            public string Response { get; set; }
+    }
+}
